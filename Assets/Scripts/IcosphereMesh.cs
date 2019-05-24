66e380165a8e3493187fe9f87abf4a6b1684e2e3
Nick Maltbie <nick.dmalt@gmail.com>
2019-05-24 12:40:03 -0400

    Now rescales the icosphere to edges are of length edgeLength.

--- Assets/Scripts/IcosphereMesh.cs
+++ Assets/Scripts/IcosphereMesh.cs
@@ -29,6 +29,18 @@ public class IcosphereMesh : MonoBehaviour
 
         List<SCoord> coordinates = new List<SCoord>(sphere.Coordinates);
         List<int> triangles = new List<int>();
+
+
+        Vector3 v1 = sphere.GetPoint(coordinates[0]);
+        IEnumerator<SCoord> scaleNeighbors = sphere.GetNeighbors(coordinates[0]).GetEnumerator();
+        scaleNeighbors.MoveNext();
+        Vector3 v2 = sphere.GetPoint(scaleNeighbors.Current);
+        float dist = Vector3.Distance(v1, v2);
+
+        float sf = edgeLength / dist;
+        sphere.SetRadius(sf);
+
+
         Vector3[] vertices = new Vector3[coordinates.Count];
         Vector3[] normals = new Vector3[coordinates.Count];
         Dictionary<SCoord, int> keyLookup = new Dictionary<SCoord, int>();
@@ -38,7 +50,6 @@ public class IcosphereMesh : MonoBehaviour
             keyLookup[coordinates[i]] = i;
             normals[i] = coordinates[i].ToEuclidian();
         }
-
         SCoordComparatorLon coordSort = new SCoordComparatorLon();
 
         for (int i = 0; i < coordinates.Count; i++)
@@ -62,9 +73,9 @@ public class IcosphereMesh : MonoBehaviour
                 {
                     SCoord[] triangleCoords = SCoord.SortClockwiseOrder(coord, nextVertex.Value, source);
 
-                    triangles.Add(keyLookup[triangleCoords[0]]);
-                    triangles.Add(keyLookup[triangleCoords[1]]);
                     triangles.Add(keyLookup[triangleCoords[2]]);
+                    triangles.Add(keyLookup[triangleCoords[1]]);
+                    triangles.Add(keyLookup[triangleCoords[0]]);
                 }
 
                 source = nextVertex.Value;
@@ -74,18 +85,18 @@ public class IcosphereMesh : MonoBehaviour
             {
                 SCoord[] triangleCoords = SCoord.SortClockwiseOrder(coord, neighbors.First.Value, source);
 
-                triangles.Add(keyLookup[triangleCoords[0]]);
-                triangles.Add(keyLookup[triangleCoords[1]]);
                 triangles.Add(keyLookup[triangleCoords[2]]);
+                triangles.Add(keyLookup[triangleCoords[1]]);
+                triangles.Add(keyLookup[triangleCoords[0]]);
             }
 
             if (keyLookup[neighbors.First.Value] > i && keyLookup[startVertex] > i)
             {
                 SCoord[] triangleCoords = SCoord.SortClockwiseOrder(coord, neighbors.First.Value, startVertex);
 
-                triangles.Add(keyLookup[triangleCoords[0]]);
-                triangles.Add(keyLookup[triangleCoords[1]]);
                 triangles.Add(keyLookup[triangleCoords[2]]);
+                triangles.Add(keyLookup[triangleCoords[1]]);
+                triangles.Add(keyLookup[triangleCoords[0]]);
             }
         }
 
@@ -102,12 +113,12 @@ public class IcosphereMesh : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        foreach (SCoord coord in sphere.Coordinates)
+        /*foreach (SCoord coord in sphere.Coordinates)
         {
             foreach (SCoord other in sphere.GetNeighbors(coord))
             {
                 Debug.DrawLine(sphere.GetPoint(coord), sphere.GetPoint(other), Color.blue);
             }
-        }
+        }*/
     }
 }
